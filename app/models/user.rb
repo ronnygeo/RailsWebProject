dc4e6421a16071e33b0265e2ba6470d14a9c9fd3
ronnygeo <ronnygeomatt@gmail.com>
2014-08-10 23:44:14 +0400

    TFA +Twilio done

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,28 +6,25 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
   has_one_time_password
 
-# put your own credentials here
-  account_sid = 'ACf2afaa9cdc187bf7eef68ee0fa49d2b3'
-  auth_token = 'f034694e4ad91d541ebb44900ea8d337'
-
-# set up a client
-  @twilio_client = ::Twilio::REST::Client.new account_sid, auth_token
-
-  @account = @twilio_client.account
-
-
   def send_two_factor_authentication_code
     puts ">>>>>>>>>>>>>>> otp_code: #{otp_code}"
     # use Model#otp_code and send via SMS, etc.
-    @twilio_client.account.sms.messages.create(
+    # put your own credentials here
+    account_sid = 'ACf2afaa9cdc187bf7eef68ee0fa49d2b3'
+    auth_token = 'f034694e4ad91d541ebb44900ea8d337'
+
+# set up a client
+    @client = Twilio::REST::Client.new account_sid, auth_token
+
+    @client.account.sms.messages.create(
         :from => '+18142976181',
         :to => '+971529245741',
         :body => "Enter this code to login to awesomeness, otp_code: #{otp_code}, Enjoy.."
     )
   end
 
-  # def need_two_factor_authentication?(request)
-  #   #request.ip != '0.0.0.0'
-  # end
+   def need_two_factor_authentication?(request)
+     request.ip != '0.0.0.0'
+   end
 
 end
